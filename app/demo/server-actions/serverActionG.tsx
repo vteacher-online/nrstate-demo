41724099aea880c781ccb2f463d06ce3a7b0b722
m-suzuki <rgbkids.g@gmail.com>
2023-05-08 16:53:37 +0900

    Add: Next.js 13 Boilerplate

--- app/demo/server-actions/serverActionG.tsx
+++ app/demo/server-actions/serverActionG.tsx
@@ -21,11 +21,15 @@ export async function serverActionDBA({
   postgres
   
   CREATE TABLE players
-  (id char(4) not null,
-  no text not null,
-  name text not null,
-  pos text not null,
-  PRIMARY KEY(id));
+  (
+    id char(4) not null,
+    no text not null,
+    name text not null,
+    pos text not null,
+    PRIMARY KEY(id)
+  );
+
+  TRUNCATE TABLE players;
   
   INSERT INTO players(id,no,name,pos) VALUES (1,'01', 'test 1' , 'pitcher');
   INSERT INTO players(id,no,name,pos) VALUES (2,'02', 'test 2' , 'pitcher');
@@ -33,8 +37,7 @@ export async function serverActionDBA({
   INSERT INTO players(id,no,name,pos) VALUES (4,'04', 'test 4' , 'catcher');
   INSERT INTO players(id,no,name,pos) VALUES (5,'05', 'test 5' , 'catcher');
   
-  truncate table players;
-*/
+  */
 
   console.log(`serverActionDBA: id=${id} name=${name} pos=${pos}`);
 
