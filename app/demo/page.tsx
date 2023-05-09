e8d4edd2c913c758cca511597bd7ab7831602503
m-suzuki <rgbkids.g@gmail.com>
2023-05-09 12:44:42 +0900

    Add: Next.js 13 Boilerplate

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -51,41 +51,46 @@ export default async function Page() {
 }
 
 async function getAdWords(a: string) {
-  // {
-  //   // test
-  //   const sleep = (time: number) =>
-  //     new Promise((resolve) => setTimeout(resolve, time));
-  //   await sleep(1000);
-  // }
+  try {
+    // {
+    //   // test
+    //   const sleep = (time: number) =>
+    //     new Promise((resolve) => setTimeout(resolve, time));
+    //   await sleep(1000);
+    // }
 
-  const result = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_API
-        ? process.env.NEXT_PUBLIC_API
-        : 'http://localhost:3000'
-    }/api/examples`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+    const result = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_API
+          ? process.env.NEXT_PUBLIC_API
+          : 'http://localhost:3000'
+      }/api/examples`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        next: { revalidate: 5 },
       },
-      next: { revalidate: 5 },
-    },
-  );
+    );
 
-  const json = await result.json();
-  const _examples = json.filter((data: { word: string; ad: string }) => {
-    return data.word.includes(a) || data.ad.includes(a);
-  });
+    const json = await result.json();
+    const _examples = json.filter((data: { word: string; ad: string }) => {
+      return data.word.includes(a) || data.ad.includes(a);
+    });
 
-  const examples = Array.from(
-    new Map(
-      _examples.map((data: { id: string; word: string; ad: string }) => [
-        data.ad,
-        { id: data.id, pos: data.ad },
-      ]),
-    ).values(),
-  );
+    const examples = Array.from(
+      new Map(
+        _examples.map((data: { id: string; word: string; ad: string }) => [
+          data.ad,
+          { id: data.id, pos: data.ad },
+        ]),
+      ).values(),
+    );
 
-  return examples;
+    return examples;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
